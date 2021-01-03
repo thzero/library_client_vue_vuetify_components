7e826f2ae9090824e77eb782bfe04dfa106c35fc
thzero <thzero@users.noreply.github.com>
2021-01-03 09:07:10 -0600

    updates

--- boot/baseValidation.js
+++ boot/baseValidation.js
@@ -1,6 +1,13 @@
 import { ValidationObserver, ValidationProvider, extend } from 'vee-validate/dist/vee-validate.full.min';
 
 class BaseValidate {
+	constructor() {
+		super();
+
+		this._regexAlphanumeric = /^[a-zA-Z0-9]+(['\-a-zA-Z0-9 ]*)*$/;
+		this._regexDecimalMap = new Map();
+	}
+
 	// eslint-disable-next-line
 	async execute(Vue, app, router, store) {
 		extend('decimal', {
@@ -10,13 +17,22 @@ class BaseValidate {
 						valid: false
 					};
 				}
+
 				if (Number(decimals) === 0) {
 					return {
 						valid: /^-?\d*$/.test(value),
 					};
 				}
+
 				const regexPart = decimals === '*' ? '+' : `{1,${decimals}}`;
-				const regex = new RegExp(`^[-+]?\\d*(\\${separator}\\d${regexPart})?([eE]{1}[-]?\\d+)?$`);
+				//const regex = new RegExp(`^[-+]?\\d*(\\${separator}\\d${regexPart})?([eE]{1}[-]?\\d+)?$`);
+				const expr = `^[-+]?\\d*(\\${separator}\\d${regexPart})?([eE]{1}[-]?\\d+)?$`;
+				let regex = this._regexDecimalMap.get(expr);
+				if (!regex) {
+					regex = new RegExp(`^[-+]?\\d*(\\${separator}\\d${regexPart})?([eE]{1}[-]?\\d+)?$`);
+					this._regexDecimalMap.set(expr, regex);
+				}
+
 				return {
 					valid: regex.test(value),
 					data: {
@@ -31,8 +47,9 @@ class BaseValidate {
 			getMessage: field => 'The ' + field + ' field may only contain alphanumeric and space characters',
 			// eslint-disable-next-line
 			validate(value, args) {
-				const regEx = /^[a-zA-Z0-9]+(['\-a-zA-Z0-9 ]*)*$/;
-				return regEx.test(value);
+				//const regEx = /^[a-zA-Z0-9]+(['\-a-zA-Z0-9 ]*)*$/;
+				//return regEx.test(value);
+				return this._regexAlphanumeric.test(value);
 			}
 		});
 
@@ -40,8 +57,9 @@ class BaseValidate {
 			getMessage: field => 'The ' + field + ' field may only contain alphanumeric, space, and punctuation characters',
 			// eslint-disable-next-line
 			validate(value, args) {
-				const regEx = /^[a-zA-Z0-9]+([',.!& \-a-zA-Z0-9 ]*)*$/;
-				return regEx.test(value);
+				// const regEx = /^[a-zA-Z0-9]+([',.!& \-a-zA-Z0-9 ]*)*$/;
+				// return regEx.test(value);
+				return this._regexAlphanumeric.test(value);
 			}
 		});
 
