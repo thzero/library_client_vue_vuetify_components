86efe1a034a433002a92b0c8394ef1e2f637de11
thzero <corvette@thzero.com>
2021-08-03 08:42:45 -0500

    update

--- components/form/VSwitchWithValidation.vue
+++ components/form/VSwitchWithValidation.vue
@@ -1,24 +1,51 @@
 <template>
-	<div>
-		<div
-			v-for="(item, index) in value"
-			:key="index"
-			class="red--text text--lighten-1 v-messages"
-		>
-			{{ item }}
-		</div>
-	</div>
+	<ValidationProvider
+		:vid="vid"
+		:name="$attrs.label"
+		:rules="rules"
+		:bail="rulesBail"
+		:immediate="true"
+	>
+		<v-switch
+      		v-model="innerValue"
+      		:label="$attrs.label"
+    	></v-switch>
+	</ValidationProvider>
 </template>
 
 <script>
+import baseControlEdit from '@/library_vue/components/baseControlEdit';
+
 export default {
 	name: 'VSwitchWithValidation',
+	extends: baseControlEdit,
 	props: {
+		rules: {
+			type: [Object, String],
+			default: ''
+		},
+		rulesBail: {
+			type: Boolean,
+			default: true
+		},
+		rulesImmediate: {
+			type: Boolean,
+			default: false
+		},
 		// must be included in props
 		value: {
-			type: Array,
-			default: () => []
+			type: Boolean,
+			default: false
 		}
+	},
+	watch: {
+		// Handles external model changes.
+		value(newVal) {
+			this.initValue(newVal);
+		}
+	},
+	mounted() {
+		this.initValue(this.value);
 	}
 };
 </script>
